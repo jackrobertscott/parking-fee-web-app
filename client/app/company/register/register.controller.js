bcabfd9abf22433ac4c75a7f3bbe825c5ab017ab
Jack Scott <mast.j.scott@gmail.com>
2015-05-09 19:33:39 +0800

    delete redundant company module code

--- client/app/company/register/register.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('webApp')
-  .controller('CompanyRegisterCtrl', function ($scope) {
-    $scope.message = 'hello';
-  });
--- client/app/company/settings/settings.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('webApp')
-  .controller('CompanySettingsCtrl', function ($scope, $state, $stateParams, Company, Auth) {
-    $scope.message = 'Hello';
-  });
