d2f31e677ea230d1e3f81ee420b510b6873cba03
jackrobertscott <mast.j.scott@gmail.com>
2015-05-10 16:53:29 +0800

    added register and settings views to location module

--- client/app/location/location.js
+++ client/app/location/location.js
@@ -7,5 +7,15 @@ angular.module('webApp')
         url: '/location',
         templateUrl: 'app/location/location.html',
         controller: 'LocationCtrl'
+      })
+      .state('locationRegister', {
+        url: '/location/register',
+        templateUrl: 'app/location/location.register.html',
+        controller: 'LocationCtrl'
+      })
+      .state('locationSettings', {
+        url: '/location/settings/:id',
+        templateUrl: 'app/location/location.settings.html',
+        controller: 'LocationCtrl'
       });
   });
