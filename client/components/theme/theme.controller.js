94886490213bb5a1130b74ae3f34c6efa3bdeb28
Jack Scott <mast.j.scott@gmail.com>
2015-05-14 15:06:08 +0800

    fix errors in Item prototype

--- client/components/theme/theme.controller.js
+++ client/components/theme/theme.controller.js
@@ -6,25 +6,25 @@ angular.module('webApp')
       // This need to fixed as it replicates the environment variable
       var userRoles = ['guest', 'user', 'inspector', 'company', 'admin'];
 
-      if (name) {this.name = name;} else {return menuError('name is not defined');}
-      if (href) {this.href = href;} else {return menuError('href is not defined');}
-      if (userRoles.indexOf(minRole) === -1) {this.minRole = minRole;} else {
-        return menuError('minRole is not valid');
+      if (name) {this.name = name;} else {return menuError('name is not defined in item');}
+      if (href) {this.href = href;} else {return menuError('href is not defined in item');}
+      if ((minRole && userRoles.indexOf(minRole) !== -1) && !minRole) {this.minRole = minRole;} else {
+        return menuError('min role is not valid in item');
       }
-      if (userRoles.indexOf(maxRole) === -1) {this.maxRole = maxRole;} else {
-        return menuError('maxRole is not valid');
+      if ((maxRole && userRoles.indexOf(maxRole) !== -1) && !maxRole) {this.maxRole = maxRole;} else {
+        return menuError('max role is not valid in item');
       }
       if (children instanceof Array) {
         this.children = children;
       } else if (children) {
-        menuError('children must be an array');
+        return menuError('children must be an array');
       }
       this.isRoleIn = function(role) {
-        if (userRoles.indexOf(role) === -1) {
+        if (userRoles.indexOf(role) !== -1) {
           return menuError('valid role was not passed to role in function');
         }
-        if (userRoles.indexOf(role) >= userRoles.indexOf(minRole) &&
-        userRoles.indexOf(role) <= userRoles.indexOf(maxRole)) {
+        if ((!this.minRole || userRoles.indexOf(role) >= userRoles.indexOf(this.minRole)) &&
+        (!this.maxRole || userRoles.indexOf(role) <= userRoles.indexOf(this.maxRole))) {
           return true;
         } else {
           return false;
@@ -32,12 +32,9 @@ angular.module('webApp')
       };
     };
 
-    $scope.menu = [];
-
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.menu = [
+      new Item('Home', '/')
+    ];
 
     $scope.logout = function() {
       Auth.logout();
