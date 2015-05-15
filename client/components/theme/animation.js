7eb23eb609dc90b60fb7acec4cdad772ef3c809d
jackrobertscott <mast.j.scott@gmail.com>
2015-05-15 16:07:57 +0800

    animations for theme work when first load for page but fail on view change

--- client/components/theme/animation.js
+++ client/components/theme/animation.js
@@ -1,16 +1,18 @@
 'use strict';
 
 $(function() {
-  // Maintain height of left section
-  $('.left').height($('#main').height());
-  // Dropdown in left section
-  $('.dropdown > a').click(function() {
-    $(this).parent().children('ul').slideToggle('fast');
-  });
-  // Dropdown in middle section
-  $('.menu-labels > li').click(function() {
-    var item = $('.menu-items').get($(this).index());
-    $('.menu-active').removeClass('menu-active').hide('fast');
-    $(item).addClass('menu-active').show('fast');
-  });
+  setTimeout(function() {
+    // Maintain height of left section
+    $('.left').height($('#main').height());
+    // Dropdown in left section
+    $('.dropdown > li').click(function() {
+      $(this).parent().children('ul').slideToggle('fast');
+    });
+    // Dropdown in middle section
+    $('.menu-labels span > li').click(function() {
+      var item = $('.menu-items').get($(this).parent().index());
+      $('.menu-active').removeClass('menu-active').hide('fast');
+      $(item).addClass('menu-active').show('fast');
+    });
+  }, 500); // For some reason, will not work without this...
 });
