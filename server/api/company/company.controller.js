aba0afaf4314fc1d01483ef605f7bd232f521370
jackrobertscott <mast.j.scott@gmail.com>
2015-05-18 21:33:09 +0800

    company node controller deletes locations associated to company on company delete #22

--- server/api/company/company.controller.js
+++ server/api/company/company.controller.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var Company = require('./company.model');
 var User = require('../user/user.model');
+var Location = require('../location/location.model');
 var config = require('../../config/environment');
 
 // Get list of companies
@@ -28,7 +29,7 @@ exports.create = function(req, res) {
     if (err) { return handleError(res, err); }
     User.findById(company._creator, function(err, user) {
       if (err) { return handleError(res, err); }
-      if (!user) { return res.send(401); }
+      if (!user) { return res.send(401); } // check
       user.company = company._id;
       if (config.userRoles.indexOf('company') > config.userRoles.indexOf(user.role)) {
         user.role = 'company';
@@ -59,22 +60,34 @@ exports.update = function(req, res) {
 exports.destroy = function(req, res) {
   Company.findById(req.params.id, function (err, company) {
     if (err) { return handleError(res, err); }
-    if (!company) { return res.send(404); }
+    if (!company) { return res.send(404); } // check
     // Remove company in users aswell
     User.find({ company: company._id }, function (err, users) {
       if (err) { return handleError(res, err); }
-      if (!users) { return res.send(404); }
+      if (!users) { return res.send(404); } // check
       users.forEach(function(user, i, array) {
         user.company = null;
-        user.role = 'user';
+        if (config.userRoles.indexOf('admin') > config.userRoles.indexOf(user.role)) {
+          user.role = 'user';
+        }
         user.save(function(err) {
           if (err) { return handleError(res, err); }
         });
       });
-      company.remove(function(err) {
-        if (err) { return handleError(res, err); }
-        return res.send(204);
-      });
+    });
+    Location.find({ company: company._id }, function (err, locations) {
+      if (err) { return handleError(res, err); }
+      if (locations && _.isArray(locations)) {
+        locations.forEach(function(location, i, array) {
+          location.remove(function(err) {
+            if (err) { return handleError(res, err); }
+          });
+        });
+      }
+    });
+    company.remove(function(err) {
+      if (err) { return handleError(res, err); }
+      return res.send(204);
     });
   });
 };
