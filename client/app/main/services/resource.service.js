a06d2b6c8f5e6ca273cbce5e986466adc4257021
jackrobertscott <mast.j.scott@gmail.com>
2015-05-22 17:52:47 +0800

    resource template updated in main module

--- client/app/main/services/resource.service.js
+++ client/app/main/services/resource.service.js
@@ -8,6 +8,12 @@
   ResourceMain.$inject = ['$resource'];
 
   function ResourceMain($resource) {
-    return $resource();
+    return $resource('/api/END_POINT_HERE/:id', {
+      id: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
   }
 })();
