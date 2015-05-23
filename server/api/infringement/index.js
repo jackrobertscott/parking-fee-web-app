1f2ce13ffbdfa1bf7480f603b4fb5830510da321
Jack Scott <mast.j.scott@gmail.com>
2015-05-23 13:19:02 +0800

    authentication added to routes #25 #26

--- server/api/infringement/index.js
+++ server/api/infringement/index.js
@@ -2,14 +2,19 @@
 
 var express = require('express');
 var controller = require('./infringement.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.hasRole('inspector'), controller.create);
+router.put('/:id', auth.hasRole('inspector'), controller.update);
+router.patch('/:id', auth.hasRole('inspector'), controller.update);
+router.delete('/:id', auth.hasRole('inspector'), controller.destroy);
+
+// Added routes
+
+router.get('/:id/company', controller.show);
 
 module.exports = router;
--- server/api/inspection/index.js
+++ server/api/inspection/index.js
@@ -2,14 +2,15 @@
 
 var express = require('express');
 var controller = require('./inspection.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.hasRole('inspector'), controller.create);
+router.put('/:id', auth.hasRole('inspector'), controller.update);
+router.patch('/:id', auth.hasRole('inspector'), controller.update);
+router.delete('/:id', auth.hasRole('inspector'), controller.destroy);
 
 module.exports = router;
