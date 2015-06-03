be312683b02b579dda9dae5a4af8725c8c9376f0
jackrobertscott <mast.j.scott@gmail.com>
2015-06-03 18:37:41 +0800

    paper cut: method rename for consistency

--- client/app/vehicle/many-vehicles.controller.js
+++ client/app/vehicle/many-vehicles.controller.js
@@ -14,7 +14,7 @@
     vm.glitch = glitch;
     vm.getMany = getMany;
     vm.remove = remove;
-    vm.getForUser = getForUser;
+    vm.getFewUser = getFewUser;
     vm.toSettings = toSettings;
 
     ////////////
@@ -50,7 +50,7 @@
       .catch(vm.glitch.handle);
     }
 
-    function getForUser() {
+    function getFewUser() {
       dataVehicle.getUserVehicles(Auth.getCurrentUser()._id)
       .then(function(items) {
         vm.items = items;
